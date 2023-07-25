24742ac799afc52df027d88def25b1c891e0b97b
有理 <youli.lx@alibaba-inc.com>
2023-07-25 14:59:41 +0800

    Implement ReadBatch() for ColumnReader

--- cpp/src/parquet/column_reader.h
+++ cpp/src/parquet/column_reader.h
@@ -219,6 +219,16 @@ class TypedColumnReader : public ColumnReader {
   virtual int64_t ReadBatch(int64_t batch_size, int16_t* def_levels, int16_t* rep_levels,
                             T* values, int64_t* values_read) = 0;
 
+  // ReadBatch for labels in GraphAr
+  virtual int64_t ReadBatch(int64_t batch_size, int32_t* repeated_nums,
+                            bool* repeated_values, int32_t& true_num, int32_t& false_num,
+                            int32_t& length, int64_t* values_read) {
+    /// ！！！ not implemented
+    throw ParquetException(
+        "ReadBatch() for GraphAr labels is not implemented in TypedColumnReader!");
+    return 0;
+  }
+
   /// Read a batch of repetition levels, definition levels, and values from the
   /// column and leave spaces for null entries on the lowest level in the values
   /// buffer.
