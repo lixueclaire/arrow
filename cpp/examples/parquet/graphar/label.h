d54fe056f6c86048ac9b3daab53cf99cfd6cbc4b
有理 <youli.lx@alibaba-inc.com>
2023-08-18 10:54:58 +0800

    Update

--- cpp/examples/parquet/graphar/label.h
+++ cpp/examples/parquet/graphar/label.h
@@ -15,6 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#ifndef PARQUET_EXAMPLES_GRAPHAR_LABEL_H
+#define PARQUET_EXAMPLES_GRAPHAR_LABEL_H
+
 #include <arrow/io/file.h>
 #include <arrow/util/logging.h>
 #include <parquet/api/reader.h>
@@ -32,64 +35,57 @@ using parquet::schema::GroupNode;
 using parquet::schema::PrimitiveNode;
 
 /// constants related to encoding and decoding of the labels
-constexpr int MAX_LABEL_NUM = 16;        // the maximum number of labels
-constexpr int MAX_DECODED_NUM = 100000;  // the maximum number of decoded values
+constexpr int MAX_LABEL_NUM = 20;         // the maximum number of labels
+constexpr int MAX_DECODED_NUM = 5000000;  // the maximum number of decoded values
 /// constants related to the parquet file
-constexpr int NUM_ROWS_PER_ROW_GROUP = 2000;  // the number of rows per row group
-constexpr int BATCH_SIZE = 1024;              // the batch size
+constexpr int NUM_ROWS_PER_ROW_GROUP = 1024 * 1024;  // the number of rows per row group
+constexpr int BATCH_SIZE = 1024;                   // the batch size
 // kDefaultDataPageSize = 1024 * 1024
 // DEFAULT_WRITE_BATCH_SIZE = 1024
 // DEFAULT_MAX_ROW_GROUP_LENGTH = 1024 * 1024
 
-/// Setup the schema of the parquet file
-static std::shared_ptr<GroupNode> SetupSchema(const int label_num,
-                                              const std::string* label_names,
-                                              const bool contain_id_column) {
-  parquet::schema::NodeVector fields;
+/// The query type
+enum QUERY_TYPE {
+  COUNT,  // return the number of valid vertices
+  INDEX,  // return the indices of valid vertices
+  BITMAP  // return the bitmap of valid vertices
+};
 
-  // Add label_num primitive nodes with specific names to the group node
-  for (int i = 0; i < label_num; ++i) {
-    // Create a primitive node with type:BOOLEAN, repetition:REQUIRED
-    if (label_names == nullptr) {
-      fields.push_back(PrimitiveNode::Make("label_" + std::to_string(i),
-                                           Repetition::REQUIRED, Type::BOOLEAN,
-                                           ConvertedType::NONE));
-    } else {
-      fields.push_back(PrimitiveNode::Make(label_names[i], Repetition::REQUIRED,
-                                           Type::BOOLEAN, ConvertedType::NONE));
-    }
-  }
-
-  if (contain_id_column) {
-    // Create a primitive node named 'id' with type:INT64, repetition:REQUIRED,
-    fields.push_back(PrimitiveNode::Make("id", Repetition::REQUIRED, Type::INT64,
-                                         ConvertedType::NONE));
-  }
-
-  // Create a GroupNode named 'schema' using the primitive nodes defined above
-  // This GroupNode is the root node of the schema tree
-  return std::static_pointer_cast<GroupNode>(
-      GroupNode::Make("schema", Repetition::REQUIRED, fields));
+/// Set bit
+static inline void SetBitmap(uint64_t* bitmap, const int index) {
+  bitmap[index >> 6] |= (1ULL << (index & 63));
 }
 
-/// Add an interval to the result
-static inline void AddToResult(std::vector<std::pair<int, int> >& intervals,
-                               const int previous_pos, const int current_pos) {
-  if (intervals.empty() || intervals.back().second != previous_pos) {
-    intervals.push_back(std::make_pair(previous_pos, current_pos));
+/// Set bit in a range
+static inline void SetBitmap(uint64_t* bitmap, const int start, const int end) {
+  int pos1 = start >> 6, pos2 = end >> 6;
+  if (pos1 == pos2) {
+    bitmap[pos1] |= (1ULL << (end & 63)) - (1ULL << (start & 63));
   } else {
-    intervals.back().second = current_pos;
+    bitmap[pos1] |= ~((1ULL << (start & 63)) - 1);
+    bitmap[pos2] |= (1ULL << (end & 63)) - 1;
+    for (int i = pos1 + 1; i < pos2; ++i) {
+      bitmap[i] = ~0ULL;
+    }
   }
+}
+
+/// Get bit
+static inline bool GetBit(const uint64_t* bitmap, const int index) {
+  return (bitmap[index >> 6]) & (1ULL << (index & 63));
 }
 
 /// Get the valid intervals of the labels, "column_number" is the number of columns
-static std::vector<std::pair<int, int> > GetValidIntervals(
-    const int column_number, const int row_number,
-    int32_t repeated_nums[][MAX_DECODED_NUM], bool repeated_values[][MAX_DECODED_NUM],
-    int32_t* length, const std::function<bool(bool*, int)>& IsValid) {
+static int GetValidIntervals(const int column_number, const int row_number,
+                             int32_t repeated_nums[][MAX_DECODED_NUM],
+                             bool repeated_values[][MAX_DECODED_NUM], int32_t* length,
+                             const std::function<bool(bool*, int)>& IsValid,
+                             std::vector<int>* indices = nullptr,
+                             uint64_t* bitmap = nullptr,
+                             const QUERY_TYPE query_type = COUNT) {
   // initialization
   std::vector<std::pair<int, int> > intervals;
-  int current_pos = 0, previous_pos = 0;
+  int current_pos = 0, previous_pos = 0, count = 0;
   int pos[MAX_LABEL_NUM] = {0};
   int index[MAX_LABEL_NUM] = {0};
   bool state[MAX_LABEL_NUM];
@@ -110,7 +106,14 @@ static std::vector<std::pair<int, int> > GetValidIntervals(
     previous_pos = current_pos;
     current_pos = min_pos;
     if (IsValid(state, column_number)) {
-      AddToResult(intervals, previous_pos, current_pos);
+      count += current_pos - previous_pos;
+      if (query_type == INDEX) {
+        for (int i = previous_pos; i < current_pos; ++i) {
+          indices->push_back(i);
+        }
+      } else if (query_type == BITMAP) {
+        SetBitmap(bitmap, previous_pos, current_pos);
+      }
     }
     // if current position is N, break the loop
     if (min_pos == row_number) {
@@ -128,21 +131,54 @@ static std::vector<std::pair<int, int> > GetValidIntervals(
     }
   }
 
-  return intervals;
+  return count;
 }
 
-std::vector<std::pair<int, int> > read_parquet_file_and_get_valid_intervals(
+int read_parquet_file_and_get_valid_indices(
     const char* parquet_filename, const int row_num, const int tot_label_num,
-    const int tested_label_num, int32_t repeated_nums[][MAX_DECODED_NUM],
-    bool repeated_values[][MAX_DECODED_NUM], int32_t* true_num, int32_t* false_num,
-    int32_t* length, const std::function<bool(bool*, int)>& IsValid);
+    const int tested_label_num, const int* tested_label_ids,
+    const std::string* label_names, const std::function<bool(bool*, int)>& IsValid,
+    std::vector<int>* indices = nullptr, uint64_t* bitmap = nullptr,
+    const QUERY_TYPE query_type = COUNT);
 
-void generate_parquet_file(const char* parquet_filename, const int row_num,
-                           const int label_num, const std::string* label_names = nullptr,
-                           const bool contain_id_column = true);
+int read_parquet_file_and_get_valid_indices(
+    const char* parquet_filename, const int row_num, const int tot_label_num,
+    const int tested_label_num, const int* tested_label_ids,
+    const std::function<bool(bool*, int)>& IsValid, std::vector<int>* indices = nullptr,
+    uint64_t* bitmap = nullptr, const QUERY_TYPE query_type = COUNT);
 
-static inline void validate_column(const int col_id, const int row_num,
+int read_parquet_file_and_get_valid_intervals(
+    const char* parquet_filename, const int row_num, const int tot_label_num,
+    const int tested_label_num, const int* tested_label_ids,
+    int32_t repeated_nums[][MAX_DECODED_NUM], bool repeated_values[][MAX_DECODED_NUM],
+    int32_t* true_num, int32_t* false_num, int32_t* length,
+    const std::function<bool(bool*, int)>& IsValid, std::vector<int>* indices = nullptr,
+    uint64_t* bitmap = nullptr, const QUERY_TYPE query_type = COUNT);
+
+void generate_parquet_file_string(const char* parquet_filename, const int row_num,
+                                  const int label_num,
+                                  bool label_column_data[][MAX_LABEL_NUM],
+                                  const std::string* label_names = nullptr,
+                                  const bool contain_id_column = true,
+                                  const bool enable_dictionary = false);
+
+void generate_parquet_file_bool_plain(const char* parquet_filename, const int row_num,
+                                      const int label_num,
+                                      bool label_column_data[][MAX_LABEL_NUM],
+                                      const std::string* label_names = nullptr,
+                                      const bool contain_id_column = true);
+
+void generate_parquet_file_bool_RLE(const char* parquet_filename, const int row_num,
+                                    const int label_num,
+                                    bool label_column_data[][MAX_LABEL_NUM],
+                                    const std::string* label_names = nullptr,
+                                    const bool contain_id_column = true);
+
+/// !!! This is only used for debug
+/* static inline void validate_column(const int col_id, const int row_num,
                                    int32_t repeated_nums[][MAX_DECODED_NUM],
                                    bool repeated_values[][MAX_DECODED_NUM],
                                    int32_t* true_num, int32_t* false_num,
-                                   int32_t* length);
+                                   int32_t* length); */
+
+#endif  // PARQUET_EXAMPLES_GRAPHAR_LABEL_H
